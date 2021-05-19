189d30db47a21ed3d3460239249af0dc679bc464
lukasz protosawicki <lukas.es.p@o2.pl>
2021-05-19 23:20:18 +0200

    add form.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
+import Footer from "./componentes/Footer.js";
 import Form from "./componentes/Form.js";
+import List from "./componentes/List.js";
 
 function App() {
   return (
     <div className="App">
       <h1>What's the Plan for Today</h1>
       <Form />
-      <ul>
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" id="" />
-            Learn to do
-          </label>
-          <button>Edit</button>
-        </li>
-      </ul>
-
-      <div className="row">
-        <label htmlFor="all">
-          <input type="checkbox" name="all" id="all" />
-          ALL
-        </label>
-        <p>You have 0 to do</p>
-        <button id="delete">Delete</button>
-      </div>
+      <List />
+      <Footer />
     </div>
   );
 }
---
+++ src/componentes/List.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <ul>
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" id="" />
+          Learn to do
+        </label>
+        <button>Edit</button>
+      </li>
+    </ul>
+  );
+};
+
+export default List;
