9d1663b5db259948c1fc4eb233e5ce869ed8c992
lukasz protosawicki <lukas.es.p@o2.pl>
2021-05-19 23:34:50 +0200

    add DataProvider.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Footer from "./componentes/Footer.js";
 import Form from "./componentes/Form.js";
 import List from "./componentes/List.js";
+import DataProvider from "./componentes/DataProvider.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's the Plan for Today</h1>
-      <Form />
-      <List />
-      <Footer />
-    </div>
+    <DataProvider>
+      <div className="App">
+        <h1>What's the Plan for Today</h1>
+        <Form />
+        <List />
+        <Footer />
+      </div>
+    </DataProvider>
   );
 }
 
--- src/componentes/List.js
+++ src/componentes/List.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import ListItem from "./ListItem";
+import { DataContext } from "./DataProvider";
 
 const List = () => {
+  const [todos, setTodos] = useContext(DataContext);
+  console.log(todos);
   return (
     <ul>
       <ListItem />
