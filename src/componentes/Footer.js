0b62356e3bf32ae3d96084ba2de2b1cef18dffa4
lukasz protosawicki <lukas.es.p@o2.pl>
2021-05-20 16:09:59 +0200

    add handleCheckAll in footer.js

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { DataContext } from "./DataProvider";
 
 const Footer = () => {
+  const [checkAll, setCheckAll] = useState(false);
+  const [todos, setTodos] = useContext(DataContext);
+
+  const handleCheckAll = () => {
+    const newTodos = [...todos];
+    newTodos.forEach((todo) => {
+      todo.complete = !checkAll;
+    });
+    setTodos(newTodos);
+    setCheckAll(!checkAll);
+  };
+
   return (
     <div className="row">
       <label htmlFor="all">
-        <input type="checkbox" name="all" id="all" />
+        <input type="checkbox" name="all" id="all" onClick={handleCheckAll} />
         ALL
       </label>
       <p>You have 0 to do</p>
