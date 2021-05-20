8abdbf28e1839285da5bf6fb3abdfe7abac26368
lukasz protosawicki <lukas.es.p@o2.pl>
2021-05-20 11:06:21 +0200

    add edit button onCLick

--- src/componentes/ListItem.js
+++ src/componentes/ListItem.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListItem = ({ todo, id, checkComplete }) => {
-  return (
-    <ul>
-      <li>
-        <label htmlFor={id} className={todo.complete ? "active" : ""}>
+  const [onEdit, setOnEdit] = useState(false);
+
+  /// edit button onClick
+  const handleOnEdit = () => {
+    setOnEdit(true);
+  };
+  if (onEdit) {
+    return (
+      <ul>
+        <li>
           <input
-            type="checkbox"
+            type="text"
             id={id}
             checked={todo.complete}
             onChange={() => checkComplete(id)}
           />
           {todo.name}
-        </label>
-        <button disabled={todo.complete} className="bt-font-size">
-          Edit
-        </button>
-      </li>
-    </ul>
-  );
+
+          <button
+            disabled={todo.complete}
+            onClick={handleOnEdit}
+            className="bt-font-size"
+          >
+            Edit
+          </button>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul>
+        <li>
+          <label htmlFor={id} className={todo.complete ? "active" : ""}>
+            <input
+              type="checkbox"
+              id={id}
+              checked={todo.complete}
+              onChange={() => checkComplete(id)}
+            />
+            {todo.name}
+          </label>
+          <button
+            disabled={todo.complete}
+            onClick={handleOnEdit}
+            className="bt-font-size"
+          >
+            Edit
+          </button>
+        </li>
+      </ul>
+    );
+  }
 };
 
 export default ListItem;
